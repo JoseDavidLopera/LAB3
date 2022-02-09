038f8b0a966573552464a9d3fc0bc70e5dcd09dc
JoseDavidLopera <87083179+JoseDavidLopera@users.noreply.github.com>
2022-02-09 10:30:29 -0500

    Add files via upload

--- primerMetodo.cpp
+++ primerMetodo.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
+#include <string.h>
+#include <string>
+#include <bitset>
 
-using namespace std;
+using namespace std; 
+ 
+void convertToASCII(string letter);
+int convertToBinary(string str);
+int main() {
+int metodo; 
+int n; 
 
-int main()
+//string text; 
+///////////////////////////////////////////////////
+/*cout<<"ingrese letras: ";
+cin>> text; 
+
+convertToASCII(text);*/
+////////////////////////////////////////////////
+
+
+//PASAR DE CARACTERES A BINARIO
+string caracteres; 
+cout<<"ingrese una cadena de caracteres: "; 
+cin>>caracteres;  
+convertToBinary(caracteres);
+
+
+
+return 0; 
+}
+
+
+
+void convertToASCII(string letter)
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    for(int i=0; i<letter.length(); i++){
+
+char x=   letter.at(i); 
+cout<<int(x)<<endl; 
+}
+}
+
+int convertToBinary(string str){
+
+for(int i = 0; i <str.size(); i ++ ){
+    bitset<8> bs4(str[i]);
+    cout<<bs4;
+   
+
+    if (i%6 == 0 && i!=0){
+        cout<<endl; 
+
+    }
+    cout<<" "; 
+    
+}
+ 
 }
